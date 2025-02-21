c7ffe343e0e0626d5b5696cfc44f4c12895a2faa
anderm4rtinez <06amartinez@sanferminikastola.eus>
2025-02-21 19:41:23 +0100

    Add files via upload

---
+++ js_pertsonalizatzailea.js
@@ -0,0 +1,41 @@
+function arrosa{
+    document.body.style.backgroundColor = "pink"
+    document.getElementById(taula).style.background = "white"
+}
+function berde{
+    document.body.style.backgroundColor = "ligthgreen"
+    document.getElementById(taula).style.background = "white"
+}
+function gris{
+    document.body.style.backgroundColor = "lightblue"
+    document.getElementById(taula).style.background = "white"
+}
+function grisa{
+    document.body.style.backgroundColor = "#444"
+    document.getElementById(taula).style.background = "white"
+}
+function grisaa{
+    document.body.style.backgroundColor = ""
+    document.getElementById(taula).style.background = "white"
+}
+function gorria{
+    document.body.style.backgroundColor = "red"
+    document.getElementById(taula).style.background = "white"
+}
+function berdea{
+    document.body.style.backgroundColor = "green"
+    document.getElementById(taula).style.background = "white"
+}
+
+function urdina{
+    document.body.style.backgroundColor = "blue"
+    document.getElementById(taula).style.background = "white"
+}
+function beltza{
+    document.body.style.backgroundColor = "black"
+    document.getElementById(taula).style.background = "white"
+}
+function txuria{
+    document.body.style.backgroundColor = "white"
+    document.getElementById(taula).style.background = "white"
+}
